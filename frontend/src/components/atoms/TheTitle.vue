0d7fe3724490a196b9ad759ab59d4cfde1e1c5b0
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-04 22:56:07 -0300

    feat: to add new atom component 'TheTitle'

---
+++ frontend/src/components/atoms/TheTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="title">{{ message }}</h1>
+</template>
+
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: rem(32);
+  color: $black;
+  font-weight: bold;
+}
+</style>
