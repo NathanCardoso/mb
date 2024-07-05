da67a91b5da3da26b0a7cc6aa5175c3bdf669788
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-04 23:26:48 -0300

    feat: to add new atom componenet Buttons

---
+++ frontend/src/components/atoms/ButtonBack.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="button" @click="buttonClick">
+    {{ text }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const buttonClick = () => {
+  this.$emit("click:go-back");
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  max-width: 100%;
+  padding: 15px 30px;
+  padding: rem(15) rem(30);
+  max-width: 100%;
+  font-size: rem(18);
+  color: $yellow;
+  border-radius: rem(8);
+  border: rem(2) solid $yellow;
+}
+</style>
---
+++ frontend/src/components/atoms/ButtonConfirm.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="button" @click="buttonClick">
+    {{ text }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const buttonClick = () => {
+  this.$emit("click:confirmed");
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  max-width: 100%;
+  padding: rem(15) rem(30);
+  background: $yellow;
+  color: $whitePrimary;
+  border: none;
+  border-radius: rem(8);
+  font-size: rem(18);
+}
+</style>
