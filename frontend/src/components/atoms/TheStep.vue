0b2db86c314e7ffff27eee5019c0c0686bdf7609
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-06 10:25:37 -0300

    feat: to add new atom component 'heStep'

---
+++ frontend/src/components/atoms/TheStep.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="step">
+    Etapa <span class="current-stage">{{ currentStage }}</span> de {{ totalStep }}
+  </p>
+</template>
+
+<script setup>
+defineProps({
+  currentStage: {
+    type: Number,
+    required: true,
+  },
+  totalStep: {
+    type: Number,
+    default: 4,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.step {
+  font-size: rem(14);
+  color: $black;
+
+  > .current-stage {
+    color: $yellow;
+  }
+}
+</style>
