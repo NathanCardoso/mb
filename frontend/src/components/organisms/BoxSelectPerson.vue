94bdd9e5ddf2a7015c1a1757bca7b32408d7f42c
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 12:50:36 -0300

    feat: created organisms BoxSelectPerson

---
+++ frontend/src/components/organisms/BoxSelectPerson.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="box-select-person">
+    <TheHeader :step="1" />
+    <SelectPerson />
+    <ButtonConfirm text="Continuar" />
+  </main>
+</template>
+
+<script setup>
+import TheHeader from "../molecules/TheHeader.vue";
+import SelectPerson from "../molecules/SelectPerson.vue";
+import ButtonConfirm from "../atoms/ButtonConfirm.vue";
+</script>
+
+<style lang="scss" scoped>
+.box-select-person {
+  display: flex;
+  flex-direction: column;
+  gap: rem(20);
+  max-width: rem(400);
+  flex-grow: 1;
+}
+</style>
