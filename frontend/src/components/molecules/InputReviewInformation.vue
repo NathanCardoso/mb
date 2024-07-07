9972dbf2a850535c7005620c3ce8a209a3d1f7ab
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 14:37:14 -0300

    feat: created molecule component 'InputReviewInformation'

---
+++ frontend/src/components/molecules/InputReviewInformation.vue
@@ -0,0 +1,18 @@
+<template>
+  <TheInput input-name="Endereço de Email" input-id="email" />
+  <TheInput input-name="Nome" input-id="name" />
+  <TheInput input-name="CNPJ" input-id="cpf" />
+  <TheInput
+    input-type="date"
+    input-name="Data de abertura"
+    inputId="data-de-nascimento"
+  />
+  <TheInput input-type="tel" input-name="Telefone" input-id="telefone" />
+  <TheInput input-name="Senha" input-id="senha" />
+</template>
+
+<script setup>
+import TheInput from "../atoms/TheInput.vue";
+</script>
+
+<style lang="scss" scoped></style>
