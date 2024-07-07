97192260d721d8598f7eae4b8ac1333818eee636
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 14:35:57 -0300

    feat: created molecule component 'InputLegalPerson'

---
+++ frontend/src/components/molecules/InputLegalPerson.vue
@@ -0,0 +1,16 @@
+<template>
+  <TheInput input-name="Nome" input-id="name" />
+  <TheInput input-name="CNPJ" input-id="cpf" />
+  <TheInput
+    input-type="date"
+    input-name="Data de abertura"
+    inputId="data-de-nascimento"
+  />
+  <TheInput input-type="tel" input-name="Telefone" input-id="telefone" />
+</template>
+
+<script setup>
+import TheInput from "../atoms/TheInput.vue";
+</script>
+
+<style lang="scss" scoped></style>
