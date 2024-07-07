b299e4c196c57f943e0acae9f7f8e11d45d49e80
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 13:59:21 -0300

    feat: created organisms component 'BoxAccessPassword'

---
+++ frontend/src/components/organisms/BoxAccessPassword.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="box-access-password">
+    <TheHeader :step="3" title-message="Senha de Acesso" />
+    <TheInput input-name="Sua senha" input-id="sua-senha" />
+    <div class="button-actions">
+      <ButtonBack text="Voltar" />
+      <ButtonConfirm text="Continuar" />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import TheHeader from "../molecules/TheHeader.vue";
+import TheInput from "../atoms/TheInput.vue";
+import ButtonBack from "../atoms/ButtonBack.vue";
+import ButtonConfirm from "../atoms/ButtonConfirm.vue";
+</script>
+
+<style lang="scss" scoped>
+.box-access-password {
+  display: flex;
+  flex-direction: column;
+  gap: rem(20);
+  max-width: rem(400);
+  flex-grow: 1;
+
+  .button-actions {
+    display: flex;
+    justify-content: space-between;
+    gap: rem(20);
+  }
+}
+</style>
