335657521a5fcc162a1fcc9d373bbb479c334a7d
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 00:22:45 -0300

    feat: created new molecule component 'TheHeader'

---
+++ frontend/src/components/molecules/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="the-header">
+    <TheStep :current-stage="step" />
+    <TheTitle message="Seja bem vindo(a)" />
+  </header>
+</template>
+
+<script setup>
+import TheStep from "../atoms/TheStep.vue";
+import TheTitle from "../atoms/TheTitle.vue";
+
+defineProps({
+  step: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.the-header {
+  display: flex;
+  flex-direction: column;
+  gap: rem(8);
+}
+</style>
