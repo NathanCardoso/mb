6e3fe2c529e510a4e06eabe5ef8d8c917486381b
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 14:39:31 -0300

    feat: created view component 'PhysicalPerson'

---
+++ frontend/src/view/PhysicalPerson.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="physical-person">
+    <BoxPhysicalPerson />
+  </div>
+</template>
+
+<script setup>
+import BoxPhysicalPerson from "@/components/organisms/BoxPhysicalPerson.vue";
+</script>
+
+<style lang="scss" scoped>
+.physical-person {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
