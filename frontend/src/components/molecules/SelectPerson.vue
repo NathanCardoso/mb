8e0624fcf26b64745d2315d56c8903ae3c4b3c37
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 12:14:42 -0300

    feat: created new molecule component 'SelectPerson'

---
+++ frontend/src/components/molecules/SelectPerson.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="select-person">
+    <TheInput input-type="email" input-name="email" />
+    <div class="input-radios">
+      <TheInputRadio input-label="Pessoa Fisica" input-name="pessoa-fisica" />
+      <TheInputRadio input-label="Pessoa Juridica" input-name="pessoa-juridica" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TheInput from "../atoms/TheInput.vue";
+import TheInputRadio from "../atoms/TheInputRadio.vue";
+</script>
+
+<style lang="scss" scoped>
+.select-person {
+  display: flex;
+  flex-direction: column;
+  gap: rem(20);
+
+  .input-radios {
+    display: flex;
+    gap: rem(20);
+  }
+}
+</style>
