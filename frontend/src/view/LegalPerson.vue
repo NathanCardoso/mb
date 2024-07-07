c4d74ba28860b42e7dbc0a17c69b253406e8a3a6
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 14:38:36 -0300

    feat: created view component LegalPerson

---
+++ frontend/src/view/LegalPerson.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="legal-person">
+    <BoxLegalPerson />
+  </div>
+</template>
+
+<script setup>
+import BoxLegalPerson from "@/components/organisms/BoxLegalPerson.vue";
+</script>
+
+<style lang="scss" scoped>
+.legal-person {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
