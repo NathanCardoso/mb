e3a39be3e00d8d688ab0d39e7d5a82b355de6c99
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 14:40:04 -0300

    feat: created view component 'ReviewInformation'

---
+++ frontend/src/view/ReviewInformation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="review-information">
+    <BoxReviewInformation />
+  </div>
+</template>
+
+<script setup>
+import BoxReviewInformation from "@/components/organisms/BoxReviewInformation.vue";
+</script>
+
+<style lang="scss" scoped>
+.review-information {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
