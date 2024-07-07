9ed20f49720e0e0ff353143782bd0580bf667b32
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-07 14:38:06 -0300

    feat: created view component 'AccessPassword'

---
+++ frontend/src/view/AccessPassword.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="legal-person">
+    <BoxAccessPassword />
+  </div>
+</template>
+
+<script setup>
+import BoxAccessPassword from "@/components/organisms/BoxAccessPassword.vue";
+</script>
+
+<style lang="scss" scoped>
+.legal-person {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
