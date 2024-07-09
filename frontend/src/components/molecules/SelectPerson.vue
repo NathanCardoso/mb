0e075e3bf606ae8eb2f28b984a141754967fc5e7
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-08 22:33:46 -0300

    feat: to add new validate for nextpage

--- frontend/src/components/molecules/SelectPerson.vue
+++ frontend/src/components/molecules/SelectPerson.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="select-person">
-    <TheInput input-type="email" input-name="email" />
+    <TheInput input-type="email" input-name="email" @input:status="inputValidate" />
     <div class="input-radios">
-      <TheInputRadio input-label="Pessoa Fisica" input-name="physical-person" />
-      <TheInputRadio input-label="Pessoa Juridica" input-name="legal-person" />
+      <TheInputRadio
+        input-label="Pessoa Fisica"
+        input-name="physical-person"
+        input-value="physical-person"
+        v-model:picked="pickedValue"
+      />
+      <TheInputRadio
+        input-label="Pessoa Juridica"
+        input-name="legal-person"
+        input-value="legal-person"
+        v-model:picked="pickedValue"
+      />
     </div>
   </div>
 </template>
@@ -11,6 +21,17 @@
 <script setup>
 import TheInput from "../atoms/TheInput.vue";
 import TheInputRadio from "../atoms/TheInputRadio.vue";
+import { ref, watch } from "vue";
+
+const emit = defineEmits(["input:status", "input:picked"]);
+
+const pickedValue = ref("");
+
+watch(pickedValue, (newValue) => emit("input:picked", newValue));
+
+const inputValidate = (status) => {
+  emit("input:status", status);
+};
 </script>
 
 <style lang="scss" scoped>
--- frontend/src/components/organisms/BoxSelectPerson.vue
+++ frontend/src/components/organisms/BoxSelectPerson.vue
@@ -1,8 +1,12 @@
 <template>
   <main class="box-select-person">
     <TheHeader :step="1" title-message="Seja bem vindo(a)" />
-    <SelectPerson />
-    <ButtonConfirm text="Continuar" @click:confirmed="nextPage" />
+    <SelectPerson @input:status="inputValidate" @input:picked="inputPickedValidate" />
+    <ButtonConfirm
+      text="Continuar"
+      :disabled="!isButtonEnable"
+      @click:confirmed="nextPage"
+    />
   </main>
 </template>
 
@@ -10,11 +14,26 @@
 import TheHeader from "../molecules/TheHeader.vue";
 import SelectPerson from "../molecules/SelectPerson.vue";
 import ButtonConfirm from "../atoms/ButtonConfirm.vue";
+import { ref, computed } from "vue";
 
 const emit = defineEmits(["select-person:next-page"]);
 
+const isEmailValid = ref(true);
+const isPickedValid = ref(true);
+const isButtonEnable = computed(
+  () => isEmailValid.value === false && isPickedValid.value === false
+);
+
 const nextPage = () => {
   emit("select-person:next-page", 2);
+};
+
+const inputValidate = (status) => {
+  isEmailValid.value = !status;
+};
+
+const inputPickedValidate = (status) => {
+  isPickedValid.value = !status;
 };
 </script>
 
