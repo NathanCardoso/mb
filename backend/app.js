b882ebdd76eafe82a64967e8ddccc8fcd6370762
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-07-10 21:32:59 -0300

    first commit back end

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/registration', (req, res) => {
+	res.status(200).send({ id: 1 })
 })
 
 app.listen(port, () => {
